88e5594724fce4bdd475997bb32d59d318f54c47
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-01 01:22:22 +0530

    Suman : POP method for Deleting first element in the linked list

--- Program.cs
+++ Program.cs
@@ -44,6 +44,14 @@ public class LinkedList
         }
     }
 
+    public void Pop()
+    {
+        if (Head != null)
+        {
+            Head = Head.Next;
+        }
+    }
+
     public void DisplayList()
     {
         Node current = Head;
@@ -62,10 +70,15 @@ public class Program
     {
         LinkedList linkedList = new LinkedList();
         linkedList.AddNode(56);
-        linkedList.AddNode(70);
         linkedList.AddNode(30);
+        linkedList.AddNode(70);
 
         Console.WriteLine("Linked List Sequence: ");
+        linkedList.DisplayList();
+
+        linkedList.Pop();
+
+        Console.WriteLine("Linked List after pop: ");
         linkedList.DisplayList();
     }
 }
